85dd3106d13ddfd399190a2788643c71fc46ccb2
hamzashoukat091 <hamzashoukat091@gmail.com>
2022-11-27 14:43:35 +0500

    Updated auto_image_generation.py

--- auto_image_generation.py
+++ auto_image_generation.py
@@ -12,12 +12,24 @@ with open(
 ) as d:
     lang_dict = [l for l in d.read().splitlines() if len(l) > 0]
 
-print(len(lang_dict))
+image_dir=os.path.join("..", os.path.split(os.path.realpath(__file__))[0], "trdg\\images")
+print(image_dir)
+# This will define the dict count for each font.
+lang_dict = lang_dict[:3]
+margins = (5, 5, 5, 5)
+# Size or format is the quality of the picture
+size = 64
+fit = False
+output_bboxes = 0
+# Note
+# For normal white background type 0, 1, 2 or 3.
+# For transparent_background type 4 and for custom_background type 5.
+# Custom background images will place in image_dir
+background_type = 5
 
-"""Load all fonts in the fonts directories
-"""
 
-generator_from_string = GeneratorFromStrings(lang_dict)
+generator_from_string = GeneratorFromStrings(strings=lang_dict, margins=margins, fit=fit, background_type=background_type,
+                                             output_bboxes=output_bboxes, size=size, image_dir=image_dir)
 
 # print(generator_from_string.fonts)
 
